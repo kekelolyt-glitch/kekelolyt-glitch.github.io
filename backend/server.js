979a55321802b73b6c6c000ee15849978888bcf1
kekelolyt-glitch <kekelolyt@gmail.com>
2026-06-21 18:29:08 +0300

    Refactor server.js to improve API structure and logging
    Removed file system dependency and updated static file serving path. Simplified login and user retrieval logic.

--- backend/server.js
+++ backend/server.js
@@ -1,73 +1,21 @@
 const express = require("express");
 const cors = require("cors");
-const fs = require("fs");
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
-app.use(express.static("frontend"));
+app.use(express.static("public"));
 
+app.use("/api/ip", require("./routes/ip"));
+app.use("/api/dns", require("./routes/dns"));
+app.use("/api/whois", require("./routes/whois"));
+app.use("/api/discord", require("./routes/discord"));
+app.use("/api/github", require("./routes/github"));
 
-app.post("/api/login",(req,res)=>{
-
-let users =
-JSON.parse(fs.readFileSync("./backend/users.json"));
-
-let user =
-users.find(x =>
-x.username == req.body.username &&
-x.password == req.body.password
-);
-
-
-if(user){
-
-return res.json({
-success:true,
-username:user.username,
-role:user.role
+app.get("/", (req,res)=>{
+res.sendFile(__dirname+"/public/index.html");
 });
 
-}
-
-
-res.json({
-success:false
-});
-
-
-});
-
-
-
-app.get("/api/status",(req,res)=>{
-
-res.json({
-
-server:"online",
-api:"active",
-version:"v1"
-
-});
-
-});
-
-
-
-app.get("/api/users",(req,res)=>{
-
-let users =
-JSON.parse(fs.readFileSync("./backend/users.json"));
-
-res.json(users);
-
-});
-
-
-
-app.listen(3000,()=>{
-
-console.log("KekeIntel Server 3000");
-
+app.listen(3000, ()=>{
+console.log("KEKEINTEL V5 RUNNING ON :3000");
 });
